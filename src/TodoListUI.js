f87787023c06b6e557301ddf0356a7f9bb867e06
Nan Guo <guonan627@hotmail.com>
2021-09-23 16:33:03 +1000

    add explanation

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -7,10 +7,10 @@ class TodoListUI extends Component {
       <div style={{marginTop:'10px', marginRight:'10px',}}>
         <div>
          <Input 
-           value={this.props.inputValue} 
+           value={this.props.inputValue}  // 以前是value={this.state.inputValue}  现在变了 从父组件props接收值
            placeholder="todo info" 
            style={{width: 300, marginRight:'10px', marginLeft:'10px'}}
-           onChange={this.props.handleInputChange}
+           onChange={this.props.handleInputChange} // 以前是onChange={this.state.handleInputChange} 现在变了 从父组件props接收方法
          />
          <Button 
            type="primary"
